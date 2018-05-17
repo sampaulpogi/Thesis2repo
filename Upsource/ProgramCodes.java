e5e9523e6367f0f1d70ffb52e08bf78d269f58e1
Paul Sam <34373228+slusampaul@users.noreply.github.com>
2018-05-17 16:49:32 +0800

    Added PrimeIneNumbers program

--- Upsource/ProgramCodes.java
+++ Upsource/ProgramCodes.java
@@ -212,6 +212,24 @@ public class Scramblies {
 }
 
 
+public class PrimeIneNumbers {
+
+    public String factors(int lst) {
+        String result = "";
+        for (int fac = 2; fac <= lst; ++fac) {
+            int count;
+            for (count = 0; lst % fac == 0; ++count) {
+                lst /= fac;
+            }
+            if (count > 0) {
+                result += "(" + fac + (count > 1 ? "**" + count : "") + ")";
+            }
+        }
+        return result;
+    }
+}
+
+
 
 
 
