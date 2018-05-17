d3460cf0d93ca81918e4b835a10f1df0ddd71e04
Paul Sam <34373228+slusampaul@users.noreply.github.com>
2018-05-17 16:52:09 +0800

    Added WhatsAPerfectPowerAnyway program

--- Upsource/ProgramCodes.java
+++ Upsource/ProgramCodes.java
@@ -346,6 +346,19 @@ public class DoubleCola {
 
 
 
+public class WhatsAPerfectPowerAnyway {
+    public int[] isPerfectPower(int n) {
+	for (int i = 2; i <= Math.sqrt(n); i++)
+        if ((Math.round((Math.log(n) / Math.log(i)) * 1000000000d) / 1000000000d) % 1 == 0)
+            return new int[] {i, (int) (Math.round((Math.log(n) / Math.log(i)) * 1000000000d) / 1000000000d)};
+
+        return null;
+
+    }
+}
+
+
+
 
 
 
