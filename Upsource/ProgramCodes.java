91a9f8bb1b024b7d840a63cb45e8e303d2078da0
Paul Sam <34373228+slusampaul@users.noreply.github.com>
2018-05-17 16:51:13 +0800

    Added TheClockwiseSpiral program

--- Upsource/ProgramCodes.java
+++ Upsource/ProgramCodes.java
@@ -273,6 +273,64 @@ public class IsMyFriendCheating {
 
 
 
+public class TheClockwiseSpiral {
+
+	private final int RIGHT = 0, DOWN = 1, LEFT = 2, UP = 3;
+
+	public  int[][] createSpiral(int n) {
+		int[][] spiral = new int[n][n];
+		int direction = RIGHT, filler = 1, xPos = 0, yPos = 0;
+		while (filler <= (n * n)) {
+			if (direction == RIGHT) {
+				if (xPos < n && spiral[yPos][xPos] == 0) {
+					spiral[yPos][xPos] = filler++;
+					xPos++;
+				} else {
+					yPos++;
+
+					xPos--;
+					direction = DOWN;
+				}
+			} else if (direction == DOWN) {
+				if (yPos < n && spiral[yPos][xPos] == 0) {
+					spiral[yPos][xPos] = filler++;
+					yPos++;
+				} else {
+					xPos--;
+
+					yPos--;
+					direction = LEFT;
+				}
+			} else if (direction == LEFT) {
+				if (xPos >= 0 && spiral[yPos][xPos] == 0) {
+					spiral[yPos][xPos] = filler++;
+					xPos--;
+				} else {
+					yPos--;
+
+					xPos++;
+					direction = UP;
+				}
+			} else if (direction == UP) {
+				if (yPos >= 0 && spiral[yPos][xPos] == 0) {
+					spiral[yPos][xPos] = filler++;
+					yPos--;
+				} else {
+					xPos++;
+
+					yPos++;
+					direction = RIGHT;
+				}
+			}
+		}
+		return spiral;
+	}
+}
+
+
+
+
+
 
 
 
